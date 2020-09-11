f6f11e3cd23cc0023119b574f584fe14ab3d0bb1
Carlos Lamosa <carlos.lamosa@gmail.com>
2020-09-11 18:31:36 +0200

    03-counter-app Finalizacion de los tests

--- 03-counter-app/src/tests/CounterApp.test.js
+++ 03-counter-app/src/tests/CounterApp.test.js
@@ -5,9 +5,14 @@ import '@testing-library/jest-dom';
 import CounterApp from '../CounterApp';
 
 describe('Pruenas en CounterApp', () => {
+
+    let wrapper = shallow(<CounterApp />); 
+    
+    beforeEach( () => {
+        wrapper = shallow(<CounterApp />);
+    })
     
     test('debe de mostrar <CounterApp /> correctamente', () => {
-        const wrapper = shallow(<CounterApp />);
         
         expect(wrapper).toMatchSnapshot();
     });
@@ -21,11 +26,57 @@ describe('Pruenas en CounterApp', () => {
 
         const valorH2 = wrapper.find('h2').text();
 
-        expect(valorH2).toBe(valor.toString());
-
-        
+        expect(valorH2).toBe(valor.toString()); 
     });
     
+    test('debe de incrementar con el botón +1', () => {
+        // const btn1 = wrapper.find('button').at(0);
+        // console.log(btn1.html());
+
+        wrapper.find('button').at(0).simulate('click')
+
+        const valor = 11;
+
+        const valorH2 = wrapper.find('h2').text();
+
+        expect(valorH2).toBe(valor.toString()); 
+
+    });
+
+    test('debe de decrementar con el botón -1', () => {
+        wrapper.find('button').at(2).simulate('click')
+
+        const valor = 9;
+
+        const valorH2 = wrapper.find('h2').text();
+
+        expect(valorH2).toBe(valor.toString()); 
+
+    });
     
+
+
+    test('debe de resetear el valor', () => {
+        const valor = 105
+        
+        const wrapper = shallow(
+            <CounterApp
+                value={valor}/>
+        );
+
+        // Hacemos click en otros botones
+        wrapper.find('button').at(2).simulate('click')
+        wrapper.find('button').at(2).simulate('click')
+        wrapper.find('button').at(2).simulate('click')
+
+        // Hacemos click en el boton de reset
+        wrapper.find('button').at(1).simulate('click')
+
+        // Obtenemos el valor que se muestra en el <h2>
+        const valorH2 = wrapper.find('h2').text();
+
+        expect(valorH2).toBe(valor.toString()); 
+
+    });
     
 })
